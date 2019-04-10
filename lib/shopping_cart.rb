b5d96e272887099c20bf4fbf222a74ce69cc8a53
Debora38 <debora.piu92@gmail.com>
2019-04-10 14:53:01 +0100

    shopping cart total is updated when items are added

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,12 +1,14 @@
 class Shopping_cart
-  attr_reader :items_in_cart
+  attr_reader :items_in_cart, :total_cost
 
   def initialize
     @items_in_cart = Array.new
+    @total_cost = 0
   end
 
   def add_to_cart(item)
     @items_in_cart << item
+    @total_cost += item.price
   end
 
   def remove_item_from_cart(item)
--- spec/unit/shopping_cart_spec.rb
+++ spec/unit/shopping_cart_spec.rb
@@ -2,17 +2,21 @@ require 'shopping_cart'
 
 RSpec.describe Shopping_cart do
   it "should add items to the cart" do
-    shoes = double('shoes')
-    allow(shoes).to receive(:name).and_return('shoes')
+    shoes = double('shoes', :name => 'shoes', :price => 99)
     subject.add_to_cart(shoes)
     expect(subject.items_in_cart[0].name).to eq 'shoes'
   end
 
   it "should remove an item from the cart" do
-    shoes = double('shoes')
-    allow(shoes).to receive(:name).and_return('shoes')
+    shoes = double('shoes', :price => 99)
     subject.add_to_cart(shoes)
     subject.remove_item_from_cart(shoes)
     expect(subject.items_in_cart).to eq []
+  end
+
+  it "should show the total of the shopping cart" do
+    shoes = double('shoes', :price => 99)
+    subject.add_to_cart(shoes)
+    expect(subject.total_cost).to eq 99
   end
 end
