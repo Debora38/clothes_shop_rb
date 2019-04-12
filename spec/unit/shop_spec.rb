bb1ef05f7e0a334849093c64275d4448207d634a
Debora38 <debora.piu92@gmail.com>
2019-04-12 16:13:46 +0100

    irrelevant tests removed - all functionalities are covered by feature testing and in the shopping cart unit tests

--- spec/unit/shop_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'shop'
-
-RSpec.describe Shop do
-  it "should create a category" do
-    subject.create_category('Womenswear')
-    expect(subject.categories[0].category_name).to eq 'Womenswear'
-  end
-end
